8a89312bf6d37ebca0907dc7717abcbdbfca294e
PratikProjecttree <pratik.vaghasiya@projecttree.in>
2024-08-08 15:32:13 +0530

    .

--- BenchmarkApp/BenchmarkHarness.cs
+++ BenchmarkApp/BenchmarkHarness.cs
@@ -13,14 +13,14 @@ namespace BenchmarkApp
         [Params(100)]
         public int IterationCount;
         private readonly ApiList _apiList = new ApiList();
-        //[Benchmark]
-        //public async Task scenario1stEntity()
-        //{
-        //    for (int i = 0; i < IterationCount; i++)
-        //    {
-        //        await _apiList.GetExecute("https://localhost:44324/Customer?$filter=SalesPerson eq 'adventure-works\\shu0'");
-        //    }
-        //}
+        [Benchmark]
+        public async Task scenario1stEntity()
+        {
+           for (int i = 0; i < IterationCount; i++)
+           {
+               await _apiList.GetExecute("https://localhost:44324/Customer?$filter=SalesPerson eq 'adventure-works\\shu0'");
+           }
+        }
         [Benchmark]
         public async Task scenario1stDapper()
         {
@@ -29,14 +29,14 @@ namespace BenchmarkApp
                 await _apiList.GetExecute("https://localhost:44324/Customer/GetFromSqlRaw?$filter=SalesPerson eq 'adventure-works\\shu0'");
             }
         }
-        //[Benchmark]
-        //public async Task scenario2ndEntity()
-        //{
-        //    for (int i = 0; i < IterationCount; i++)
-        //    {
-        //        await _apiList.GetExecute("https://localhost:44324/Customer?includeAddresses=true&$filter=CustomerAddresses/any(address: address/CountryRegion eq 'United States') and CustomerAddresses/any(address: address/StateProvince eq 'Texas')");
-        //    }
-        //}
+        [Benchmark]
+        public async Task scenario2ndEntity()
+        {
+           for (int i = 0; i < IterationCount; i++)
+           {
+               await _apiList.GetExecute("https://localhost:44324/Customer?includeAddresses=true&$filter=CustomerAddresses/any(address: address/CountryRegion eq 'United States') and CustomerAddresses/any(address: address/StateProvince eq 'Texas')");
+           }
+        }
         [Benchmark]
         public async Task scenario2ndDapper()
         {
@@ -45,14 +45,14 @@ namespace BenchmarkApp
                 await _apiList.GetExecute("https://localhost:44324/Customer/GetFromSqlRaw?includeAddresses=true&$filter=CustomerAddresses/any(address: address/CountryRegion eq 'United States') and CustomerAddresses/any(address: address/StateProvince eq 'Texas')");
             }
         }
-        //[Benchmark]
-        //public async Task scenario3rdEntity()
-        //{
-        //    for (int i = 0; i < IterationCount; i++)
-        //    {
-        //        await _apiList.GetExecute("https://localhost:44324/Customer?includeSalesOrderHeaders=true&$filter=SalesOrderHeaders/any(soh: soh/SalesOrderDetails/any(sod: sod/Product/Name eq 'Sport-100 Helmet, Red'))&$orderby=customerId asc");
-        //    }
-        //}
+        [Benchmark]
+        public async Task scenario3rdEntity()
+        {
+           for (int i = 0; i < IterationCount; i++)
+           {
+               await _apiList.GetExecute("https://localhost:44324/Customer?includeSalesOrderHeaders=true&$filter=SalesOrderHeaders/any(soh: soh/SalesOrderDetails/any(sod: sod/Product/Name eq 'Sport-100 Helmet, Red'))&$orderby=customerId asc");
+           }
+        }
         [Benchmark]
         public async Task scenario3rdDapper()
         {
@@ -61,14 +61,14 @@ namespace BenchmarkApp
                 await _apiList.GetExecute("https://localhost:44324/Customer/GetFromSqlRaw?includeSalesOrderHeaders=true&$filter=SalesOrderHeaders/any(soh: soh/SalesOrderDetails/any(sod: sod/Product/Name eq 'Sport-100 Helmet, Red'))&$orderby=customerId asc");
             }
         }
-        //[Benchmark]
-        //public async Task scenario4thEntity()
-        //{
-        //    for (int i = 0; i < IterationCount; i++)
-        //    {
-        //        await _apiList.GetExecute("https://localhost:44324/Customer?$filter=SalesOrderHeaders/any(soh: soh/SalesOrderDetails/any(sod: sod/Product/ProductCategory eq 'Helmets'))&includeSalesOrderHeaders=true");
-        //    }
-        //}
+        [Benchmark]
+        public async Task scenario4thEntity()
+        {
+           for (int i = 0; i < IterationCount; i++)
+           {
+               await _apiList.GetExecute("https://localhost:44324/Customer?$filter=SalesOrderHeaders/any(soh: soh/SalesOrderDetails/any(sod: sod/Product/ProductCategory eq 'Helmets'))&includeSalesOrderHeaders=true");
+           }
+        }
         [Benchmark]
         public async Task scenario4thDapper()
         {
